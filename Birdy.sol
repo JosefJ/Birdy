d86338689fd77a3c19f0f2eadcb86b5079c3ac8e
josef <josef.jelacic@gmail.com>
2017-12-25 20:34:40 +0100

    type check fix

--- Birdy.sol
+++ Birdy.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 import './Ownable.sol';
+import './SafeMath.sol';
 
 contract BirdyLog is Ownable {
     // Event declarations
@@ -10,73 +11,77 @@ contract BirdyLog is Ownable {
     event birdsUpdated(uint256 newBirds);
     event paidOut (address indexed breeder, uint256 amount);
     event breederChanged (address indexed breeder);
+    event payoutChanged (uint256 payout);
 
     // Event triggers
-    function newDonor(address donor) {
+    function NEW_DONOR (address donor) public {
         newDonor(donor);
     }
 
-    function newDonation(address donor, uint256 weiDonated) {
+    function NEW_DONATION (address donor, uint256 weiDonated) public {
         newDonation(donor, weiDonated);
     }
 
-    function birdyStack(address log, address donations, address feeder) {
+    function BIRDY_STACK (address log, address donations, address feeder) public {
         birdyStack(log, donations, feeder);
     }
 
-    function birdsUpdated(uint256 newBirds) {
+    function BIRDS_UPDATED (uint256 newBirds) public {
         birdsUpdated(newBirds);
     }
 
-    function paidOut (address breeder, uint256 amount) {
+    function PAID_OUT (address breeder, uint256 amount) public {
         paidOut (breeder, amount);
     }
 
-    function breederChanged (address breeder) {
+    function BREEDER_CHANGED (address breeder) public {
         breederChanged (breeder);
     }
 
+    function PAYOUT_CHANGED (uint256 payout) public {
+        payoutChanged(payout);
+    }
 }
 
 contract BirdyDonations {
 
-    uint256 weiDontated;
+    uint256 weiDonated;
     uint256 donorsCounter;
 
     BirdyLog log;
     Birdy feeder;
 
     mapping (address => uint256) donated;
-    mapping (uint32 => address) donorsList;
+    mapping (uint256 => address) donorsList;
 
     function BirdyDonations(Birdy _feeder, BirdyLog _log) public {
         feeder = _feeder;
         log = _log;
     }
 
-    function () payable {
+    function () payable public {
         require(msg.value > 0);
-        if (donated[msg.origin] = 0) {
-            donorsList[donorsCounter] = msg.origin;
+        if (donated[tx.origin] == 0) {
+            donorsList[donorsCounter] = tx.origin;
             donorsCounter += 1; // Will potentially overflow, but that's ok
-            log.newDonor(msg.origin);
+            log.NEW_DONOR(tx.origin);
         }
         weiDonated += msg.value; // Will potentially overflow, but that's ok
-        donated[msg.origin] += msg.value; // Will potentially overflow, but that's ok
+        donated[tx.origin] += msg.value; // Will potentially overflow, but that's ok
         forwardFunds();
-        log.newDonation(msg.origin, msg.value);
+        log.NEW_DONATION(tx.origin, msg.value);
     }
 
-    function forwardFunds() payable internal {
+    function forwardFunds() internal {
         feeder.transfer(msg.value);
     }
 }
 
 contract Birdy is Ownable{
-    using SafeMath for uin256;
+    using SafeMath for uint256;
 
     BirdyLog log;
-    BirdyDontations donations;
+    BirdyDonations donations;
 
     mapping (address => uint256) streak;
 
@@ -91,30 +96,36 @@ contract Birdy is Ownable{
 
     function Birdy() {
         log = new BirdyLog();
-        donations = new BirdyDonations(log);
-        log.birdyStack(log, donations, this);
+        donations = new BirdyDonations(this, log);
+        log.BIRDY_STACK(log, donations, this);
     }
 
-    function () possible {
-        uint256 memory timeDiff = now - timeChanged;
+    function () public payable{
+        if (msg.sender == address(donations)) {
+            // do nothing
+        }
+        
+        else {
+            uint256 timeDiff = now - timeChanged;
 
-        require (timeDiff >= 3600);
-        require (breeder != msg.sender);
+            require (timeDiff >= 3600);
+            require (breeder != msg.sender);
 
-        streak[breeder] += timeDiff;
+            streak[breeder] += timeDiff;
 
-        payOutNow();
+            payOutNow();
 
-        breeder = msg.sender;
-        timeChanged = now;
+            breeder = msg.sender;
+            timeChanged = now;
 
-        log.breederChanged(msg.sender);
+            log.BREEDER_CHANGED(msg.sender);
+        }
     }
 
-    function iterateBirds(uint256 birds) onlyOwner {
+    function iterateBirds(uint256 birds) public onlyOwner {
         birdsSince += birds;
         birdsTotal += birds;
-        log.birdsUpdated(birds);
+        log.BIRDS_UPDATED(birds);
     }
 
     function payOut() public onlyOwner {
@@ -124,7 +135,7 @@ contract Birdy is Ownable{
         breeder.transfer(payout);
         // TODO: "Error" handling
 
-        log.paidOut(breeder, payout);
+        log.PAID_OUT(breeder, payout);
     }
 
     function payOutNow() internal {
@@ -134,13 +145,13 @@ contract Birdy is Ownable{
         breeder.transfer(payout);
         // TODO: "Error" handling
 
-        log.paidOut(breeder, payout);
+        log.PAID_OUT(breeder, payout);
     }
 
     // Settings
     function changePayout(uint256 _new) public onlyOwner {
         payPerBird = _new;
-        log.payOutChanged(_new);
+        log.PAYOUT_CHANGED(_new);
     }
 }
 
